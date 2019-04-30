b9d12fc49b4a5923cf7e3cb7a49aaa8c0887d187
Kiyotaka <>
2019-04-30 18:52:14 +0900

    2-1

--- src/main/scala/Lesson2/OddOccurrencesInArray.scala
+++ src/main/scala/Lesson2/OddOccurrencesInArray.scala
@@ -1,5 +1,14 @@
 package Lesson2
 
-class OddOccurrencesInArray {
-
+object OddOccurrencesInArray {
+  def solution(a: Array[Int]): Int = {
+    a.foldLeft(Set[Int]()){ (stock, num) =>
+      stock.contains(num) match {
+        case true => stock - num
+        case false => stock + num
+      }
+    }.head
+  }
 }
+
+// https://app.codility.com/demo/results/trainingCEAU63-TV5/
